OpenMandrivaAssociation/uClibc

commit 4b8ea1739bfeb8438eac67c9436cec15845a1fed

SILENT: make sure that -Os always overrides default -O2

uClibc.spec
@@ -154,7 +154,7 @@ Small libc for building embedded applications.
 %else
 %define arch_cflags %nil
 %endif
-%global	cflags	%{optflags} -std=gnu99 %{ldflags} -muclibc -fuse-ld=bfd %{arch_cflags}
+%global	cflags	%{optflags} -Os -std=gnu99 %{ldflags} -muclibc -fuse-ld=bfd %{arch_cflags}
 
 sed %{SOURCE2} \
 %ifarch %{arm}

commit 01c6c96cf8610556c00dbda48f1640d8293f160d

SILENT: redo 4b8ea1739bfeb8438eac67c9436cec15845a1fed, accidentally SILENT: reverted by mdawkins in fd016bd9c4142bc245c64e4cf3df4f693e9c5354

uClibc.spec
@@ -155,7 +155,7 @@ Small libc for building embedded applications.
 %else
 %define arch_cflags %nil
 %endif
-%global	cflags	%{optflags} -std=gnu99 %{ldflags} -muclibc -Wl,-rpath=%{uclibc_root}/%{_lib} -Wl,-rpath=%{uclibc_root}%{_libdir} -fuse-ld=bfd %{arch_cflags}
+%global	cflags	%{optflags} -Os -std=gnu99 %{ldflags} -muclibc -Wl,-rpath=%{uclibc_root}/%{_lib} -Wl,-rpath=%{uclibc_root}%{_libdir} -fuse-ld=bfd %{arch_cflags}
 
 sed %{SOURCE2} \
 %ifarch %{arm}